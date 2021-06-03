967cd2b04242d419e61967af7ec6dffb42e51fab
dogef <lovme.amir@gmail.com>
2021-06-03 17:29:49 +0400

    first commit

--- server.js
+++ server.js
@@ -25,7 +25,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function (req, res) {
+  console.log(req);
+  let headers = req.rawHeaders;
+  res.json({'ipaddress':req.ip,'language':headers[7],'software':headers[3]});
+  });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
